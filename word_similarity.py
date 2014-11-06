14d0cb3cbc344b1d131d091e7051e22b059a195f
scarafoni <dan@scarafoni.com>
2014-11-05 23:22:30 -0500

    cn similarity

--- word_similarity.py
+++ word_similarity.py
@@ -1,6 +1,7 @@
 # code from
 # http://sujitpal.blogspot.com/2013/02/checking-for-word-equivalence-using.html
 from __future__ import division
+import requests
 from nltk.corpus import wordnet as wn
 import itertools as it
 import sys
@@ -12,12 +13,12 @@ def vec_semantic_sim(v1,v2):
     sim_acc = 0.0
     compares = 0 
     for (w1, w2) in it.product(v1, v2):
-        sim_acc += similarity(w1,w2)
+        sim_acc += wordnet_similarity(w1,w2)
         compares += 1
     return sim_acc/ float(compares)
          
     
-def similarity(w1, w2, sim=wn.path_similarity):
+def wordnet_similarity(w1, w2, sim=wn.path_similarity):
   synsets1 = wn.synsets(w1)
   synsets2 = wn.synsets(w2)
   sim_scores = []
@@ -28,6 +29,10 @@ def similarity(w1, w2, sim=wn.path_similarity):
     return 0
   else:
     return max(sim_scores)
+
+def cn_similarity(w1, w2):
+    req = requests.get('http://conceptnet5.media.mit.edu/data/5.2/assoc/c/en/'+w1+'?filter=/c/en/'+w2+'&limit=1')
+    return req.json()['similar'].split(',')[1]
 
 def main():
     (word1, word2) = ("large", "big")
